de1576cca75ebe7f9cffaeac32a8fceb1a108a42
Tertius Scheepers <Tertiussc@gmail.com>
2020-08-17 08:13:58 +0200

    completed lesson 181: Authenticate web token

--- src/index.js
+++ src/index.js
@@ -3,15 +3,12 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const authRoutes = require('./routes/authRoutes')
+const requireAuth = require('./middlewares/requireAuth')
 
 const app = express()
 
 app.use(bodyParser.json())
 app.use(authRoutes)
-
-app.get('/', (req, res) => {
-    res.send('Hi there!')
-})
 
 mongoUri = 'mongodb+srv://admin:athena01@trackserver.zqgbt.mongodb.net/<dbname>?retryWrites=true&w=majority'
 mongoose.connect(mongoUri, {
@@ -25,6 +22,11 @@ mongoose.connection.on('connected', () => {
 mongoose.connection.on('error', (err) => {
     console.error('Error connecting to mongo', err)
 })
+
+app.get('/', requireAuth, (req, res) => {
+    res.send(`Your email: ${req.user.email}`)
+})
+
 
 app.listen(3000, () => {
     console.log('Listining on port 3000')
